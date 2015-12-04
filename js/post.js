2c012a90fa21ea7543a3a2cf12d16188411f0048
Mark Rossmair <tuc72616@temple.edu>
2015-12-04 16:17:08 -0500

    Update post.js

--- js/post.js
+++ js/post.js
@@ -1,4 +1,5 @@
 $(function() {
+    Alert("POOP!")
     var shareButtonConfig = {
         description: (window.POST_TITLE ? ('"' + window.POST_TITLE + '" via @skeswa') : undefined) || 'A Cool Post on Sandile\'s Blog'
     };
@@ -6,9 +7,11 @@ $(function() {
     new Share('#share-button', shareButtonConfig);
     // Show the share button
     setTimeout(function() {
+        Alert("POOP!")
         $('#share-button').removeClass('concealed');
     }, 1000);
     setTimeout(function() {
+        Alert("POOP!")
         $('#share-button').addClass('visible');
     }, 1200);
     // Highlight the code segments
